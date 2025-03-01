dbe3ed0fd086797cc0553d3425f6b5af9fedf1cb
arth2004 <134796751+arth2004@users.noreply.github.com>
2025-03-01 14:09:49 +0530

    fix : CORS issue

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://book-it-tau.vercel.app/",
   })
 );
 

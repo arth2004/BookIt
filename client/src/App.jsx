ed5db2b0b089495976661f2f9dbcd978a21060f6
Arth <arthagrawal72@gmail.com>
2025-06-06 18:45:54 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ import { BookingPage } from "./Pages/BookingPage";
 import { BookingsPage } from "./Pages/BookingsPage";
 import { useEffect, useState } from "react";
 
-// axios.defaults.baseURL = "https://bookit-ianc.onrender.com";
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = process.env.BACKEND_BASE_URL;
+// axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
 
 function App() {
